a1a088624a1c2098092818745d2e26942cc4531e
Tristan Da Costa Pinto <tristandacostapinto@gmail.com>
2021-02-28 17:08:36 +0100

    List of heroes

---
+++ src/app/heroes-list.ts
@@ -0,0 +1,32 @@
+import {
+  Hero
+} from './hero';
+export const HEROES: Hero[] = [{
+  id: 1,
+  name: 'Water Mage',
+  element: 'Water'
+}, {
+  id: 2,
+  name: 'Fire Mage',
+  element: 'Fire'
+}, {
+  id: 3,
+  name: 'Air Mage',
+  element: 'Air'
+}, {
+  id: 4,
+  name: 'Earth Mage',
+  element: 'Earth'
+}, {
+  id: 5,
+  name: 'Thunder Mage',
+  element: 'Thunder'
+}, {
+  id: 6,
+  name: 'Shadow Mage',
+  element: 'Shadow'
+}, {
+  id: 7,
+  name: 'Light Mage',
+  element: 'Light'
+}]
--- src/app/heroes-of-light/heroes-of-light.component.ts
+++ src/app/heroes-of-light/heroes-of-light.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../heroes-list';
 
 @Component({
   selector: 'app-heroes',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes-of-light.component.css']
 })
 export class HeroesOfLightComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 hero : Hero = {
   id: 1,
   name: 'Water Mage',
